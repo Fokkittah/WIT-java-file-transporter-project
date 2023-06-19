47e7ead73ef0a2def7b3206602313500a333c5cd
GitMateo <tech.api.mail@gmail.com>
2023-06-19 20:32:45 +0200

    Testowe kopiowanie
    Jest to branch z maina z dodanymi maskami i nie działającym kopiowaniem :)

--- src/main/java/view/AppWindow.java
+++ src/main/java/view/AppWindow.java
@@ -6,70 +6,149 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
-import java.awt.BorderLayout;
 import javax.swing.JSeparator;
-import java.awt.*;
+import javax.swing.JTextField;
+import javax.swing.JOptionPane;
+import javax.swing.JFileChooser;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 
 /**
  * Class responsible for setting up the GUI
- * @author Fokkittah 
  */
-
- public class AppWindow {
+public class AppWindow {
 
     private JFrame frame;
     private JButton sourceButton;
     private JButton destinationButton;
     private JButton startButton;
+    private JTextField fileMaskField;
+    private File sourceDirectory;
+    private File destinationDirectory;
+    private JLabel statusLabel;
 
     public AppWindow() {
-        frame = new JFrame(); // creates a new JFrame
-        frame.setTitle("Multithreaded Copy App"); // sets the title of the window
-        frame.setSize(500, 300); // sets the initial size of the window
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // closes the application when the window is closed
+        frame = new JFrame();
+        frame.setTitle("Multithreaded Copy App");
+        frame.setSize(500, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // The above is just a simple window
-        // ----------------------------
-        // Adding functionality below...
+        frame.setLayout(new BorderLayout());
 
-        frame.setLayout(new BorderLayout()); // use border layout
+        JPanel northButtonPanel = new JPanel();
+        northButtonPanel.setLayout(new BoxLayout(northButtonPanel, BoxLayout.LINE_AXIS));
 
-        JPanel northButtonPanel = new JPanel(); // create a new panel for the buttons
-        northButtonPanel.setLayout(new BoxLayout(northButtonPanel, BoxLayout.LINE_AXIS)); // set the layout to line up components from left to right
+        sourceButton = new JButton("Select Source");
+        destinationButton = new JButton("Select Destination");
+        startButton = new JButton("Start");
+        fileMaskField = new JTextField("*.*");
 
-        sourceButton = new JButton("Select Source"); // create source button
-        destinationButton = new JButton("Select Destination"); // create destination button
-        startButton = new JButton("Start"); // create start button
+        northButtonPanel.add(sourceButton);
+        northButtonPanel.add(destinationButton);
+        northButtonPanel.add(Box.createHorizontalGlue());
+        northButtonPanel.add(startButton);
 
-        northButtonPanel.add(sourceButton); // add source button to the panel
-        northButtonPanel.add(destinationButton); // add destination button to the panel
-        northButtonPanel.add(Box.createHorizontalGlue()); // add "glue" to take up extra space
-        northButtonPanel.add(startButton); // add start button to the panel
+        frame.add(northButtonPanel, BorderLayout.NORTH);
 
-        frame.add(northButtonPanel, BorderLayout.NORTH); // add the button panel to the north
-
-        // Add a separator
         JPanel separatorPanel = new JPanel(new BorderLayout());
         separatorPanel.add(new JSeparator(), BorderLayout.NORTH);
         frame.add(separatorPanel, BorderLayout.CENTER);
 
-        // Add a separator and panel to the bottom
         JPanel bottomPanel = new JPanel(new BorderLayout());
-        bottomPanel.setBackground(Color.LIGHT_GRAY); // set the background color of the bottom panel
-        
-        // Add a separator to the top of the bottom panel
+        bottomPanel.setBackground(Color.LIGHT_GRAY);
+
         bottomPanel.add(new JSeparator(), BorderLayout.NORTH);
 
-        // Add a status label to the right of the bottom panel
-        JLabel statusLabel = new JLabel("Waiting for source input...");
+        statusLabel = new JLabel("Waiting for source input...");
         statusLabel.setHorizontalAlignment(JLabel.RIGHT);
         bottomPanel.add(statusLabel, BorderLayout.EAST);
 
-        frame.add(bottomPanel, BorderLayout.SOUTH); // add the bottom panel to the south
+        frame.add(bottomPanel, BorderLayout.SOUTH);
+
+        sourceButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                selectSourceDirectory();
+            }
+        });
+
+        destinationButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                selectDestinationDirectory();
+            }
+        });
+
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                startCopying();
+            }
+        });
     }
 
+    private void selectSourceDirectory() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int result = fileChooser.showOpenDialog(frame);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            sourceDirectory = fileChooser.getSelectedFile();
+            statusLabel.setText("Source Selected: " + sourceDirectory.getAbsolutePath());
+        }
+    }
+
+    private void selectDestinationDirectory() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int result = fileChooser.showOpenDialog(frame);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            destinationDirectory = fileChooser.getSelectedFile();
+            statusLabel.setText("Destination Selected: " + destinationDirectory.getAbsolutePath());
+        }
+    }
+
+    private void startCopying() {
+        String fileMask = fileMaskField.getText();
+
+        if (sourceDirectory == null || destinationDirectory == null) {
+            JOptionPane.showMessageDialog(frame, "Please select source and destination directories", "Error", JOptionPane.ERROR_MESSAGE);
+        } else if (!sourceDirectory.exists() || !sourceDirectory.isDirectory()) {
+            JOptionPane.showMessageDialog(frame, "Invalid source directory", "Error", JOptionPane.ERROR_MESSAGE);
+        } else if (!destinationDirectory.exists() || !destinationDirectory.isDirectory()) {
+            JOptionPane.showMessageDialog(frame, "Invalid destination directory", "Error", JOptionPane.ERROR_MESSAGE);
+        } else {
+            // Perform file copying logic here
+            File[] filesToCopy = sourceDirectory.listFiles();
+
+            // Create a thread pool with the specified number of threads
+            int numThreads = 4;
+            ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
+
+            for (File file : filesToCopy) {
+                if (file.isFile() && file.getName().matches(fileMask)) {
+                    executorService.execute(new FileCopyTask(file, destinationDirectory));
+                }
+            }
+
+            executorService.shutdown();
+
+            try {
+                executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            int copiedFilesCount = FileCopyTask.getCopiedFilesCount();
+            statusLabel.setText("Copied files count: " + copiedFilesCount);
+        }
+    }
+
+
     public void show() {
-        frame.setVisible(true); // makes the window visible
+        frame.setVisible(true);
     }
 }
